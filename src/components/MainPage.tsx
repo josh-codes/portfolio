dcbee03822f1588da6afd284eb2c203cb1834b6f
Josh <joshua.brest99@gmail.com>
2022-07-13 17:05:34 -0700

    Fix the linter

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -5,28 +5,43 @@ export const MainPage = () => {
 	return (
 		<article className='flex flex-col gap-10 pt-4'>
 			<section>
-				<h1 className='font-bold text-5xl pb-8'>{config.title}</h1>
+				<h1 className='pb-8 text-5xl font-bold'>{config.title}</h1>
 				<p>{config.description}</p>
 			</section>
 			<section>
-				<h2 className='font-bold text-3xl pb-4'>My projects</h2>
+				<h2 className='pb-4 text-3xl font-bold'>My projects</h2>
 				<p>Bellow are some of the projects I made.</p>
-				{config.work.items.length === 0 && (<>
-					<div className='w-full p-5 grid place-items-center text-xl'>
-						{config.work.defaultText}
-					</div>
-				</>)}
+				{config.work.items.length === 0 && (
+					<>
+						<div className='grid w-full place-items-center p-5 text-xl'>
+							{config.work.defaultText}
+						</div>
+					</>
+				)}
 				<div className='grid grid-cols-2'>
 					{config.work.items.map((item, index) => {
 						return (
-							<article key={index} className='border-gray-200 border grid'>
-								<img src={item.image} alt={item.image_alt} className='h-20 w-full '/>
-								<div className='bg-gray-900 bg-opacity-40 h-20 w-full mt-[-5rem] flex'>
-									<h3 className='font-bold text-2xl pl-5 pb-2 grid mt-auto'>{item.title}</h3>
+							<article
+								key={index}
+								className='grid border border-gray-200'
+							>
+								<img
+									src={item.image}
+									alt={item.image_alt}
+									className='h-20 w-full '
+								/>
+								<div className='mt-[-5rem] flex h-20 w-full bg-gray-900 bg-opacity-40'>
+									<h3 className='mt-auto grid pl-5 pb-2 text-2xl font-bold'>
+										{item.title}
+									</h3>
 								</div>
 								<div className='p-5 pt-1'>
 									<p className='pb-2'>{item.description}</p>
-									<a className='p-3 pt-2 pb-2 cursor-pointer bg-gray-800 hover:bg-gray-700' href={item.link} target={'work_blank_'+index}>
+									<a
+										className='cursor-pointer bg-gray-800 p-3 pt-2 pb-2 hover:bg-gray-700'
+										href={item.link}
+										target={'work_blank_' + index}
+									>
 										View Project
 									</a>
 								</div>
@@ -36,34 +51,46 @@ export const MainPage = () => {
 				</div>
 			</section>
 			<section>
-				<h2 className='font-bold text-3xl pb-4'>Technologies</h2>
+				<h2 className='pb-4 text-3xl font-bold'>Technologies</h2>
 				<p>Bellow are some of the Technologies I use.</p>
-				{config.tech.items.length === 0 && (<>
-					<div className='w-full p-5 grid place-items-center text-xl'>
-						{config.contact.defaultText}
-					</div>
-				</>)}
+				{config.tech.items.length === 0 && (
+					<>
+						<div className='grid w-full place-items-center p-5 text-xl'>
+							{config.contact.defaultText}
+						</div>
+					</>
+				)}
 				<ul className='columns-[3] pt-2'>
 					{config.tech.items.map((item, index) => {
-						return (
-							<li key={index}>{item}</li>
-						);
+						return <li key={index}>{item}</li>;
 					})}
 				</ul>
 			</section>
 			<section>
-				<h2 className='font-bold text-3xl pb-4'>Find me</h2>
-				{config.contact.items.length === 0 && (<>
-					<div className='w-full p-5 grid place-items-center text-xl'>
-						{config.contact.defaultText}
-					</div>
-				</>)}
+				<h2 className='pb-4 text-3xl font-bold'>Find me</h2>
+				{config.contact.items.length === 0 && (
+					<>
+						<div className='grid w-full place-items-center p-5 text-xl'>
+							{config.contact.defaultText}
+						</div>
+					</>
+				)}
 				{config.contact.items.map((item, index) => {
 					return (
 						<div key={index} className='flex'>
-							<label className='font-bold pr-2'>{item.text}:</label>
-							{item.link && (<a className='underline' href={item.link} target={'contact_blank_'+index}>{item.value}</a>)}
-							{!item.link && (<span>{item.value}</span>)}
+							<label className='pr-2 font-bold'>
+								{item.text}:
+							</label>
+							{item.link && (
+								<a
+									className='underline'
+									href={item.link}
+									target={'contact_blank_' + index}
+								>
+									{item.value}
+								</a>
+							)}
+							{!item.link && <span>{item.value}</span>}
 						</div>
 					);
 				})}
--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -6,23 +6,29 @@ export const SEO = () => {
 		<>
 			{/* Primary Meta Tags */}
 			<title>{config.seo.title}</title>
-			<meta name='title' content={config.seo.title}/>
-			<meta name='description' content={config.seo.description}/>
-			<meta name='keywords' content={config.seo.keywords.join(', ')}/>
-			<meta name='robots' content='index,follow'/>
-			<meta name='language' content='English'/>
-			<meta name='revisit-after' content={config.seo.crawl_every + ' days'}/>
-			<meta name='author' content={config.seo.author}/>
+			<meta name='title' content={config.seo.title} />
+			<meta name='description' content={config.seo.description} />
+			<meta name='keywords' content={config.seo.keywords.join(', ')} />
+			<meta name='robots' content='index,follow' />
+			<meta name='language' content='English' />
+			<meta
+				name='revisit-after'
+				content={config.seo.crawl_every + ' days'}
+			/>
+			<meta name='author' content={config.seo.author} />
 			{/* Open Graph / Facebook */}
-			<meta property='og:type' content='website'/>
-			<meta property='og:url' content={config.seo.url}/>
-			<meta property='og:title' content={config.seo.title}/>
-			<meta property='og:description' content={config.seo.description}/>
+			<meta property='og:type' content='website' />
+			<meta property='og:url' content={config.seo.url} />
+			<meta property='og:title' content={config.seo.title} />
+			<meta property='og:description' content={config.seo.description} />
 			{/* Twitter */}
-			<meta property='twitter:card' content='summary_large_image'/>
-			<meta property='twitter:url' content={config.seo.url}/>
-			<meta property='twitter:title' content={config.seo.title}/>
-			<meta property='twitter:description' content={config.seo.description}/>
+			<meta property='twitter:card' content='summary_large_image' />
+			<meta property='twitter:url' content={config.seo.url} />
+			<meta property='twitter:title' content={config.seo.title} />
+			<meta
+				property='twitter:description'
+				content={config.seo.description}
+			/>
 		</>
 	);
 };
