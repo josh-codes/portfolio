1dff78757fadc49f2a661bddc1dc4e0744f93d93
Josh <joshua.brest99@gmail.com>
2022-07-13 18:36:58 -0700

    Make readerview work and make change some wording.

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -4,16 +4,16 @@ import config from '../config.json';
 export const MainPage = () => {
 	return (
 		<article className='flex flex-col gap-10 pt-4'>
+			<h1 className='pb-8 text-5xl font-bold'>{config.title}</h1>
 			<section>
-				<h1 className='pb-8 text-5xl font-bold'>{config.title}</h1>
+				<h2 className='pb-4 text-3xl font-bold'>About Me</h2>
 				<p>{config.description}</p>
 			</section>
 			<section>
-				<h2 className='pb-4 text-3xl font-bold'>My projects</h2>
-				<p>Bellow are some of the projects I made.</p>
+				<h2 className='pb-4 text-3xl font-bold'>Projects</h2>
 				{config.work.items.length === 0 && (
 					<>
-						<div className='grid w-full place-items-center p-5 text-xl'>
+						<div className='grid w-full place-items-center border-2 border-dashed border-gray-200 p-5 text-xl'>
 							{config.work.defaultText}
 						</div>
 					</>
@@ -31,19 +31,25 @@ export const MainPage = () => {
 									className='h-20 w-full '
 								/>
 								<div className='mt-[-5rem] flex h-20 w-full bg-gray-900 bg-opacity-40'>
-									<h3 className='mt-auto grid pl-5 pb-2 text-2xl font-bold'>
-										{item.title}
-									</h3>
+									<section>
+										<h3 className='mt-auto grid pl-5 pb-2 text-2xl font-bold'>
+											{item.title}
+										</h3>
+									</section>
 								</div>
 								<div className='p-5 pt-1'>
-									<p className='pb-2'>{item.description}</p>
-									<a
-										className='cursor-pointer bg-gray-800 p-3 pt-2 pb-2 hover:bg-gray-700'
-										href={item.link}
-										target={'work_blank_' + index}
-									>
-										View Project
-									</a>
+									<section>
+										<p className='pb-2'>
+											{item.description}
+										</p>
+										<a
+											className='cursor-pointer bg-gray-800 p-3 pt-2 pb-2 hover:bg-gray-700'
+											href={item.link}
+											target={'work_blank_' + index}
+										>
+											View Project
+										</a>
+									</section>
 								</div>
 							</article>
 						);
@@ -55,8 +61,8 @@ export const MainPage = () => {
 				<p>Bellow are some of the Technologies I use.</p>
 				{config.tech.items.length === 0 && (
 					<>
-						<div className='grid w-full place-items-center p-5 text-xl'>
-							{config.contact.defaultText}
+						<div className='grid w-full place-items-center border-2 border-dashed border-gray-200 p-5 text-xl'>
+							{config.tech.defaultText}
 						</div>
 					</>
 				)}
@@ -70,17 +76,15 @@ export const MainPage = () => {
 				<h2 className='pb-4 text-3xl font-bold'>Find me</h2>
 				{config.contact.items.length === 0 && (
 					<>
-						<div className='grid w-full place-items-center p-5 text-xl'>
+						<div className='grid w-full place-items-center border-2 border-dashed border-gray-200 p-5 text-xl'>
 							{config.contact.defaultText}
 						</div>
 					</>
 				)}
 				{config.contact.items.map((item, index) => {
 					return (
-						<div key={index} className='flex'>
-							<label className='pr-2 font-bold'>
-								{item.text}:
-							</label>
+						<p className='pr-2' key={index}>
+							<b>{item.text}: </b>
 							{item.link && (
 								<a
 									className='underline'
@@ -91,7 +95,7 @@ export const MainPage = () => {
 								</a>
 							)}
 							{!item.link && <span>{item.value}</span>}
-						</div>
+						</p>
 					);
 				})}
 			</section>
